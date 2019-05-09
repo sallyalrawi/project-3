00303c763a949b7dc160e3e3632ff31341a7208b
crcass <ccass@live.com>
2019-05-08 21:07:55 -0500

    initial databse setup

---
+++ models/diary.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+  const Diary = sequelize.define('Diaries', {
+    user_id: DataTypes.STRING,
+    meal: DataTypes.STRING,
+    description: DataTypes.STRING,
+    calories: DataTypes.INTEGER
+  });
+  return Diary;
+};
---
+++ models/weight.js
@@ -0,0 +1,9 @@
+// Figure out how to create new table for each user
+
+module.exports = (sequelize, DataTypes) => {
+  const Weight = sequelize.define('Weights', {
+    user_id: DataTypes.STRING,
+    weight: DataTypes.INTEGER
+  });
+  return Weight;
+};
