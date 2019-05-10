1d14737fbcbeab0a898dcfdd723e26487bda668e
sally <sallyakrum83@gmail.com>
2019-05-10 18:22:10 -0500

    trying to fix stuff

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,53 @@ import React from 'react'
 // eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
-import Login from './components/Login'
+import LogIn from './components/Login'
 import SignUp from './components/SignUp'
 // eslint-disable-next-line no-unused-vars
 import { AuthProvider } from './Auth'
 // eslint-disable-next-line no-unused-vars
-import PrivateRout from './privateRoute'
-class App extends React.Component {
-  render () {
-    return (
-      <AuthProvider>
-        <Router>
-          <div>
-            <PrivateRout exact path = "/" component={Home} authenticated={this.state.authenticated} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-          </div>
-        </Router>
-      </AuthProvider>
+import privateRoute from './privateRoute'
+class App extends Component {
+  state = { loading: true, authenticated: false, user: null };
 
-    )
+  render() {
+    const { authenticated, loading } = this.state;
+
+    if (loading) {
+      return <p>Loading..</p>;
+    }
+
+    return (
+      <Router>
+        <div>
+          <PrivateRoute
+            exact
+            path="/"
+            component={Home}
+            authenticated={authenticated}
+          />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    );
   }
 }
-
+componentWillMount(){
+  app.auth().onAuthStateChanged(user => {
+    if (user) {
+      this.setState({
+        authenticated: true,
+        currentUser: user,
+        loading: false
+      });
+    } else {
+      this.setState({
+        authenticated: false,
+        currentUser: null,
+        loading: false
+      });
+    }
+  });
+}
 export default App
