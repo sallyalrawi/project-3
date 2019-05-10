5c8cb8866d64cbf2923465fdcba3bb0897deb795
sally <sallyakrum83@gmail.com>
2019-05-10 14:15:17 -0500

    added the components home signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 /* eslint-disable import/no-duplicates */
-
 import React from 'react'
 // eslint-disable-next-line no-unused-vars
-import index from './components/SignUp/index'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
 // eslint-disable-next-line no-unused-vars
-import SignUp from './components/SignUp/index'
-// import { BrowserRouter as Router} from 'react-router-dom'
-
+import { AuthProvider } from './Auth'
+// eslint-disable-next-line no-unused-vars
+import PrivateRout from './privateRoute'
 class App extends React.Component {
   render () {
     return (
-      <SignUp/>
+      <AuthProvider>
+        <Router>
+          <div>
+            <PrivateRout exact path = "/" component={Home} authenticated={this.state.authenticated} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+          </div>
+        </Router>
+      </AuthProvider>
+
     )
   }
 }
---
+++ client/src/firebase.js
@@ -0,0 +1,15 @@
+
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+// eslint-disable-next-line no-unused-vars
+const app = firebase.initializeApp({
+  apiKey: 'AIzaSyD3v0txe-b3QoLTpuACEIcLeD1zW0M6WwI',
+  authDomain: 'project3-auth-b6344.firebaseapp.com',
+  databaseURL: 'https://project3-auth-b6344.firebaseio.com',
+  projectId: 'project3-auth-b6344',
+  storageBucket: 'project3-auth-b6344.appspot.com',
+  messagingSenderId: '760468695211',
+  appId: '1:760468695211:web:f065bc4595ae4446'
+})
+
+export default app
