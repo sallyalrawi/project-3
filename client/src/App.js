e66f414bcac6129776cab7b92d387931afb81a3e
sally <sallyakrum83@gmail.com>
2019-05-10 11:19:11 -0500

    install firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
+/* eslint-disable import/no-duplicates */
 
 import React from 'react'
-import Signup from './components/containers/Signup'
+// eslint-disable-next-line no-unused-vars
+import index from './components/SignUp/index'
+// eslint-disable-next-line no-unused-vars
+import SignUp from './components/SignUp/index'
 // import { BrowserRouter as Router} from 'react-router-dom'
 
 class App extends React.Component {
   render () {
     return (
-      <Signup />
+      <SignUp/>
     )
   }
 }
