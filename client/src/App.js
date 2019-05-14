847aca463084d210955e085467382a5012eac629
Taylor F Stiger <taylorfgomez@gmail.com>
2019-05-13 21:29:37 -0500

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,12 @@
 /* eslint-disable import/no-duplicates */
 import React, { Component } from 'react'
-// eslint-disable-next-line no-unused-vars
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './components/Home'
-import LogIn from './components/Login'
-import SignUp from './components/SignUp'
-// eslint-disable-next-line no-unused-vars
-import { AuthProvider } from './Auth'
-// eslint-disable-next-line no-unused-vars
-import PrivateRoute from './privateRoute'
-import app from './firebase'
+import DiaryForm from "./components/DiaryForm/"
 class App extends Component {
-  state = { loading: true, authenticated: false, user: null };
-
-  componentWillMount(){
-    // app.auth().onAuthStateChanged(user => {
-    //   if (user) {
-    //     this.setState({
-    //       // authenticated: true,
-    //       // currentUser: user,
-    //       loading: false
-    //     });
-    //   } else {
-    //     this.setState({
-    //       // authenticated: false,
-    //       // currentUser: null,
-    //       loading: false
-    //     });
-    //   }
-    // });
-  }
-
   render() {
-    const { authenticated, loading } = this.state;
-
-    // if (loading) {
-    //   return <p>Loading..</p>;
-    // }
-
     return (
-      <AuthProvider>
-        <Router>    
-        <div>
-          <PrivateRoute exact path="/" component={Home} />
-          <Route exact path="/login" component={LogIn} />
-          <Route exact path="/signup" component={SignUp} />
-        </div>
-       </Router>
-      </AuthProvider>
+      <div>
+        <DiaryForm />
+      </div>
     );
   }
 }
---
+++ client/src/components/DiaryForm/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.css'
+
+const DiaryForm = ({ handleChange, handleSubmit, value }) => {
+  return (
+    <div className="main">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="searchBar">Search by Food Name</label>
+        <input onChange={handleChange} type="text"
+          name="foodKeyword" value={value} />
+        <button>Search</button>
+      </form>
+
+
+    </div>
+  );
+}
+
+export default DiaryForm;
